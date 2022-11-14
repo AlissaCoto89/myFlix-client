a25e506f66afbd3f969034ee71d677301f997df4
AlissaCoto89 <acoto189@gmail.com>
2022-11-14 12:10:30 -0500

    fixed file structure

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function Navbar({ user }) {
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open("/", "_self");
+  };
+
+  const isAuth = () => {
+    if (typeof window == "undefined") {
+      return false;
+    }
+    if (localStorage.getItem("token")) {
+      return localStorage.getItem("token");
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <Navbar
+      className="main-nav"
+      sticky="top"
+      bg="dark"
+      expand="lg"
+      variant="dark"
+    >
+      <Container>
+        <Navbar.Brand className="navbar-logo" as={Link} to="/">
+          MyFlix
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            {isAuth() && (
+              <Nav.Link as={Link} to={`/users/${user}`}>
+                {user}
+              </Nav.Link>
+            )}
+            {isAuth() && (
+              <Nav.Link as={Link} to="/" onClick={onLoggedOut}>
+                Logout
+              </Nav.Link>
+            )}
+            {!isAuth() && (
+              <Nav.Link as={Link} to="/">
+                Login
+              </Nav.Link>
+            )}
+            {!isAuth() && (
+              <Nav.Link as={Link} to="/register">
+                Sign Up
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
